7b4aa4a04fa44951a83e9f83554de6489f25aa08
Maciej Czapiewski <mcjczapiewski@gmail.com>
2020-03-21 16:28:43 +0100

    Add add(a,b) function

--- gitting.py
+++ gitting.py
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
-print('hello, Im gitty by Michal')
-=======
 def introduce():
 	print("Hello, I'm Gittie!")
->>>>>>> ac17242b4cb14af01c682fff9efc4d65af907a18
+
+def add(a, b):
+	return a + b
